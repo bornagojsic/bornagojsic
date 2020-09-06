1777e5c0fd9e8f50e1324200c06aaddb0d07805e
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2020-09-06 20:24:42 +0200

    Adding starfield.js

---
+++ starfield.js
@@ -0,0 +1,69 @@
+function Star() {
+  this.x = random(-width, width);
+  this.y = random(-height, height);
+  this.z = random(width);
+  this.pz = this.z;
+
+  this.update = function() {
+    this.z = this.z - speed;
+    if (this.z < 1) {
+      this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.pz = this.z;
+    }
+  }
+
+  this.show = function() {
+    fill(255);
+    noStroke();
+
+    var sx = map(this.x / this.z, 0, 1, 0, width);
+    var sy = map(this.y / this.z, 0, 1, 0, height);
+
+    var r = map(this.z, 0, width, 16, 0);
+    ellipse(sx, sy, r, r);
+
+    var px = map(this.x / this.pz, 0, 1, 0, width);
+    var py = map(this.y / this.pz, 0, 1, 0, height);
+
+    this.pz = this.z;
+
+    stroke(255);
+    line(px, py, sx, sy);
+
+  }
+}
+
+var stars = [];
+
+var speed;
+
+const minSpeed = 10;
+const maxSpeed = 50;
+
+function setup() {
+  cnv = createCanvas(windowWidth - 20, 650);
+  cnv.parent('canvas');
+  for (var i = 0; i < 800; i++) {
+    stars[i] = new Star();
+  }
+}
+
+function draw() {
+  if ( ( window.innerWidth >= 800 ) && ( window.innerHeight >= 600 ) && (mouseY < 700) )  {
+    if (mouseY < height/2) {
+      speed = map(mouseY, 0, height/2, minSpeed, maxSpeed, height);
+    } else {
+      speed = map(mouseY, height/2, height, maxSpeed, minSpeed, height);
+    }
+  } else {
+    speed = minSpeed;
+  }
+  background('#0b0c10');
+  translate(width / 2, height / 2);
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
